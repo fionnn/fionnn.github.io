31ffe038e214edfb7294616f77af6de1ead7e256
fionn <30909179+fionnn@users.noreply.github.com>
2019-01-20 20:42:22 +0000

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,19 +1,46 @@
-const clock = document.getElementById('clock');
+(function ($) {
+    /**
+     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
+     * according to the GMT+0 Timezone
+     **/
+    var launch = new Date(2014, 11, 04, 12, 00);
+    /**
+     * The script
+     **/
+    var message = $('#message');
+    var days = $('#days');
+    var hours = $('#hours');
+    var minutes = $('#minutes');
+    var seconds = $('#seconds');
 
-function getDate() {
-    const date = new Date();
-    const seconds = date.getSeconds();
-    const minutes = date.getMinutes();
-    const hours = date.getHours() %12;
-    const time = hours.toString().padStart(2, '0') + ':' +
-    minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+    setDate();
 
-    return time;
+    function setDate() {
+        var now = new Date();
+        if (launch < now) {
+            days.html('<h1>0</H1><p>Day</p>');
+            hours.html('<h1>0</h1><p>Hour</p>');
+            minutes.html('<h1>0</h1><p>Minute</p>');
+            seconds.html('<h1>0</h1><p>Second</p>');
+            message.html('coming soon...');
+        } else {
+            var s = -now.getTimezoneOffset() * 60 + (launch.getTime() - now.getTime()) / 1000;
+            var d = Math.floor(s / 86400);
+            days.html('<h1>' + d + '</h1><p>Day' + (d > 1 ? 's' : ''), '</p>');
+            s -= d * 86400;
 
-}
+            var h = Math.floor(s / 3600);
+            hours.html('<h1>' + h + '</h1><p>Hour' + (h > 1 ? 's' : ''), '</p>');
+            s -= h * 3600;
 
-clock.innerHTML = getDate();
+            var m = Math.floor(s / 60);
+            minutes.html('<h1>' + m + '</h1><p>Minute' + (m > 1 ? 's' : ''), '</p>');
 
-setInterval(function() {
-    clock.innerHTML=getDate();
-}, 1000);
+            s = Math.floor(s - m * 60);
+            seconds.html('<h1>' + s + '</h1><p>Second' + (s > 1 ? 's' : ''), '</p>');
+            setTimeout(setDate, 1000);
+
+            message.html('coming soon...');
+        }
+    }
+})(jQuery);
