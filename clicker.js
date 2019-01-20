45eee4a6689b8bb37b80f0c197cfbea661723e74
fionn <30909179+fionnn@users.noreply.github.com>
2019-01-20 22:28:58 +0000

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -3,4 +3,7 @@ function clickerCounter()
 {
   clicker++
   document.getElementById("clickerCounter").innerHTML = "you have clicked " + clicker + " times.";
+  if
+    document.getElementById("clickerCounter").innterHTML = "you have clicked 2 times.";
+            <p id="hiddenMessage8" style="display:none">oo 2</p>  
 }
