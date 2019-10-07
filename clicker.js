09bf5cfcbf11bb947d1c60c49bd1fc39631545fb
fionn <30909179+fionnn@users.noreply.github.com>
2019-10-07 13:50:16 +0100

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -3,8 +3,4 @@ function clickerCounter()
 {
   clicker++
   document.getElementById("clickerCounter").innerHTML = "you have clicked " + clicker + " times.";
-  if
-    document.getElementById("clickerCounter").innterHTML = "you have clicked 2 times.";
-          <button onclick="revealMessage8()" class="button button4">more features</button>
-            <p id="hiddenMessage8" style="display:none">oo 2</p>  
 }
