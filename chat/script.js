2dac085740425fe107eba0539a5250e00dcf96e4
fionn <fionn.tf@gmail.com>
2023-09-13 10:41:21 +0100

    realtime

--- chat/script.js
+++ chat/script.js
@@ -10,5 +10,19 @@ function sendMessage() {
         messages.appendChild(messageElement);
         messageInput.value = "";
         messages.scrollTop = messages.scrollHeight;
+
+        // Send the message to the server using WebSocket
+        websocket.send(message);
     }
 }
+
+// Connect to the WebSocket server
+const websocket = new WebSocket("wss://ws.postman-echo.com/raw");
+
+// Handle incoming messages from the server
+websocket.addEventListener("message", function (event) {
+    const messageElement = document.createElement("div");
+    messageElement.textContent = event.data;
+    document.getElementById("messages").appendChild(messageElement);
+    document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
+});
